5ec627cd2cc20a698d94155e6790d1acfe4e4c73
andrenunes0010 <148992437+andrenunes0010@users.noreply.github.com>
2024-11-28 14:13:35 +0000

    Login

---
+++ app/src/main/java/com/example/appmovel_pis/ApiService.kt
@@ -0,0 +1,16 @@
+package com.example.appmovel_pis
+
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+// Modelo para a requisição de login
+data class LoginRequest(val email: String, val senha: String)
+
+// Modelo para a resposta do login
+data class LoginResponse(val id: Int, val nome: String, val email: String)
+
+interface ApiService {
+    @POST("login")
+    suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
+}
