a33a52f528931f8ab79e0c3a1d50686608cbb5fe
andrenunes0010 <148992437+andrenunes0010@users.noreply.github.com>
2024-12-11 15:19:07 +0000

    App começa na welcome page e redireciona para o logn

--- app/src/main/java/com/example/appmovel_pis/WelcomePage.kt
+++ app/src/main/java/com/example/appmovel_pis/WelcomePage.kt
@@ -1,6 +1,8 @@
 package com.example.appmovel_pis
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class WelcomePage : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val getStartedButton = findViewById<Button>(R.id.GetStartedBtn)
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
