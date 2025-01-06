01c57ec5bc11d1bccb2b1ef2d806d56599fd837f
D4rkL3ss <miguektiago93@gmail.com>
2025-01-06 22:27:25 +0000

    .

--- app/src/main/java/com/example/appmovel_pis/ui/fragments/Menu.kt
+++ app/src/main/java/com/example/appmovel_pis/ui/fragments/Menu.kt
@@ -1,10 +1,12 @@
 package com.example.appmovel_pis.ui.fragments
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.appmovel_pis.R
 
 class MenuFragment : Fragment() {
@@ -13,7 +15,20 @@ class MenuFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
+
+        // Reference the ImageView for Sensores
+        val sensoresImageView = view.findViewById<ImageView>(R.id.SensoresBTN)
+
+        // Set the click listener
+        sensoresImageView.setOnClickListener {
+            // Replace with SensoresFragment
+            val transaction = parentFragmentManager.beginTransaction()
+            transaction.replace(R.id.menuFragmentContainer, SensorFragment())
+            transaction.addToBackStack(null) // Allow back navigation
+            transaction.commit()
+        }
+
+        return view
     }
 }
--- app/src/main/java/com/example/appmovel_pis/ui/menu/MenuPage.kt
+++ app/src/main/java/com/example/appmovel_pis/ui/menu/MenuPage.kt
@@ -27,10 +27,13 @@ class MenuPage : AppCompatActivity() {
                 .commit()
         }
 
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
 }
